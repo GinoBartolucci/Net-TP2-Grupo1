1dcfe1e27282ad669ba9047062d2ca47ee1c0b0a
Gino Bartolucci <ginobartolucci00@gmail.com>
2021-06-24 11:10:08 -0300

    LAB03

---
+++ Labs/TPL03/TP2/Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+namespace Business.Logic
+{
+    public class UsuarioLogic: BusinessLogic
+    {
+        private Data.Database.UsuarioAdapter UsuarioData;
+        public UsuarioLogic()
+        {
+            UsuarioData = new UsuarioAdapter();
+        }
+        public void GetOne(int id)
+        {
+            Business.Entities.Usuario.
+        }
+        public List<Usuario> GetAll()
+        {
+            return UsuarioData.GetAll();
+        }
+        public void Save()
+        {
+
+        }
+        public void Delete()
+        {
+
+        }
+    }
+}
