6b79bd7d3337dd88f648b8e0fd5fedf7d1c727a2
Gino Bartolucci <ginobartolucci00@gmail.com>
2021-06-25 02:52:47 -0300

    TPL03

---
+++ Labs/TPL04/TP2/Business.Entities/Modulo.cs
@@ -0,0 +1,13 @@
+﻿namespace Business.Entities
+{
+    public class Modulo : BusinessEntity
+    {
+        private string _Descripcion;
+
+        public string Descripcion
+        {
+            get { return _Descripcion; }
+            set { _Descripcion = value; }
+        }
+    }
+}
