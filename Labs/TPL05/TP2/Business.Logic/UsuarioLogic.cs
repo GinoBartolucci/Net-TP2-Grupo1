d9566c03ba9284f0401c0ab14649518a3d54d17b
Gino Bartolucci <ginobartolucci00@gmail.com>
2021-06-26 19:43:14 -0300

    TPL05
    Creación lab05 tp2

---
+++ Labs/TPL05/TP2/Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+namespace Business.Logic
+{
+    public class UsuarioLogic: BusinessLogic
+    {
+        public UsuarioLogic()
+        {
+            UsuarioData = new UsuarioAdapter();
+        }
+        private Data.Database.UsuarioAdapter UsuarioData;
+        
+        public Business.Entities.Usuario GetOne(int id)
+        {
+            return UsuarioData.GetOne(id);
+        }
+        public List<Usuario> GetAll()
+        {
+            return UsuarioData.GetAll();
+        }
+        public void Save(Business.Entities.Usuario u)
+        {
+            UsuarioData.Save(u);
+        }
+        public void Delete(int id)
+        {
+            UsuarioData.Delete(id);
+        }
+    }
+}
---
+++ Labs/TPL05/TP2/UI.Desktop/Usuarios.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Business.Logic;
+using Business.Entities;
+
+namespace UI.Desktop
+{
+    public partial class Usuarios : Form
+    {
+        public Usuarios()
+        {
+            InitializeComponent();
+            this.dgvUsuarios.AutoGenerateColumns = false;
+        }
+
+        private void toolStripContainer1_TopToolStripPanel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        public void Listar()
+        {
+            UsuarioLogic ul = new UsuarioLogic();
+            this.dgvUsuarios.DataSource = ul.GetAll();
+
+            id.DataPropertyName = "ID";
+            nombre.DataPropertyName = "Nombre";
+            apellido.DataPropertyName = "Apellido";
+            usuario.DataPropertyName = "NombreUsuario";
+            email.DataPropertyName = "EMail";
+            habilitado.DataPropertyName = "Habilitado";
+        }
+
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnActualizar_Click(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void tlUsuarios_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void tsbNuevo_Click(object sender, EventArgs e)
+        {
+            UsuarioDesktop ud = new UsuarioDesktop(ApplicationForm.ModoForm.Alta);
+            ud.ShowDialog();
+            Listar();
+        }
+
+        private void tsbEdiar_Click(object sender, EventArgs e)
+        {
+            if (dgvUsuarios.SelectedRows != null)
+            {
+                int id = ((Business.Entities.Usuario)dgvUsuarios.SelectedRows[0].DataBoundItem).ID;
+                UsuarioDesktop ud = new UsuarioDesktop(ApplicationForm.ModoForm.Modificacion, id);
+                ud.ShowDialog();
+                Listar();
+            }
+            else if(dgvUsuarios.SelectedRows == null)
+            {
+                MessageBox.Show("Error", "Seleccione un Usuario\n para edirar", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void tsbEliminar_Click(object sender, EventArgs e)
+        {
+            int id = ((Business.Entities.Usuario)dgvUsuarios.SelectedRows[0].DataBoundItem).ID;
+            UsuarioDesktop ud = new UsuarioDesktop(ApplicationForm.ModoForm.Baja, id);
+            ud.ShowDialog();
+            Listar();
+        }
+    }
+}
